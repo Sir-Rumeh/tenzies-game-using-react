eae913a4079b8e4550ea7855e36b6eaf9feeb860
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2022-06-27 03:19:16 +0100

    update changes

--- src/InfoBar.js
+++ src/InfoBar.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
-export default function InfoBar({clicks}) {
-  return (
-    <div className='info-bar'>
-      <h2>You have made {clicks} moves</h2>
-    
-    </div>
-  )
+export default function InfoBar({ clicks }) {
+	return (
+		<div className="info-bar">
+			<h2>You have made {clicks} moves</h2>
+			<script>{console.log("hello")}</script>
+		</div>
+	);
 }
