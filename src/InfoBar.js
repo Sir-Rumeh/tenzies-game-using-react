fff8e9736d19e0894120a9229587f18aa64a0419
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2022-06-30 21:02:53 +0100

    update changes

--- src/InfoBar.js
+++ src/InfoBar.js
@@ -4,7 +4,7 @@ export default function InfoBar({ clicks }) {
 	return (
 		<div className="info-bar">
 			<h2>You have made {clicks} moves</h2>
-			<script>{console.log("hello")}</script>
+			{/* <script>{console.log("hello")}</script> */}
 		</div>
 	);
 }
